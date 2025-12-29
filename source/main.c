94069803b45ae4eaaa2c3429cde63e252065b750
AzizBgBoss <mohamedazizbenghezala64@gmail.com>
2025-12-29 01:32:55 +0100

    Added sprite

--- source/main.c
+++ source/main.c
@@ -2,6 +2,7 @@
 #include <nds.h>
 
 #include "tilemap.h"
+#include "sprites.h"
 
 #define WORLD_WIDTH 16
 #define WORLD_HEIGHT 12
@@ -10,8 +11,22 @@
 #define TILE_TREE 1
 #define TILE_ROCK 2
 
+#define DIR_UP 0
+#define DIR_DOWN 1
+#define DIR_LEFT 2
+#define DIR_RIGHT 3
+
 int bg0;
 uint16_t *bg0Map;
+
+struct Player
+{
+    int x;
+    int y;
+    int direction;
+};
+
+struct Player player = {0, 0, DIR_DOWN};
 
 uint8_t worldTerrain[WORLD_WIDTH][WORLD_HEIGHT];
 
@@ -35,16 +50,39 @@ int main(int argc, char **argv)
 {
     videoSetMode(MODE_0_2D);
 
-    vramSetPrimaryBanks(VRAM_A_MAIN_BG, VRAM_B_LCD, VRAM_C_LCD, VRAM_D_LCD);
+    vramSetPrimaryBanks(VRAM_A_MAIN_BG, VRAM_B_MAIN_SPRITE, VRAM_C_LCD, VRAM_D_LCD);
 
     bg0 = bgInit(0, BgType_Text8bpp, BgSize_T_256x256, 0, 1);
 
     dmaCopy(tilemapTiles, bgGetGfxPtr(bg0), tilemapTilesLen);
     dmaCopy(tilemapPal, BG_PALETTE, tilemapPalLen);
 
+    bgSetPriority(bg0, 3);
+
     bg0Map = (uint16_t *)bgGetMapPtr(bg0);
 
     dmaFillHalfWords(TILE_EMPTY, bg0Map, 32 * 32 * 2);
+
+    oamInit(&oamMain, SpriteMapping_1D_128, false);
+
+    // Allocate space for the tiles and copy them there
+    u16 *playerGfx = oamAllocateGfx(&oamMain, SpriteSize_16x16, SpriteColorFormat_256Color);
+    dmaCopy(spritesTiles, playerGfx, 8 * 8 * 4 * 2); // Tile size X * Y * 4 tiles * 2 bytes (u16)
+
+    // Copy palette
+    dmaCopy(spritesPal, SPRITE_PALETTE, spritesPalLen);
+
+    oamSet(&oamMain, 0,
+           0, 0, // X, Y
+           0, // Priority
+           0, // Palette index
+           SpriteSize_16x16, SpriteColorFormat_256Color, // Size, format
+           playerGfx,  // Graphics offset
+           -1, // Affine index
+           false, // Double size
+           false, // Hide
+           false, false, // H flip, V flip
+           false); // Mosaic
 
     consoleDemoInit();
 
@@ -74,9 +112,35 @@ int main(int argc, char **argv)
     {
         swiWaitForVBlank();
 
+        oamSetXY(&oamMain, 0, player.x, player.y);
+        oamUpdate(&oamMain);
+
         scanKeys();
-        if (keysHeld() & KEY_START)
+
+        int held = keysHeld();
+        if (held & KEY_START)
             break;
+        
+        if (held & KEY_UP){
+            player.y--;
+            player.direction = DIR_UP;
+            dmaCopy(spritesTiles + 8 * 8 * 4 * player.direction, playerGfx, 8 * 8 * 4 * 2);
+        }
+        if (held & KEY_DOWN){
+            player.y++;
+            player.direction = DIR_DOWN;
+            dmaCopy(spritesTiles + 8 * 8 * 4 * player.direction, playerGfx, 8 * 8 * 4 * 2);
+        }
+        if (held & KEY_LEFT){
+            player.x--;
+            player.direction = DIR_LEFT;
+            dmaCopy(spritesTiles + 8 * 8 * 4 * player.direction, playerGfx, 8 * 8 * 4 * 2);
+        }
+        if (held & KEY_RIGHT){
+            player.x++;
+            player.direction = DIR_RIGHT;
+            dmaCopy(spritesTiles + 8 * 8 * 4 * player.direction, playerGfx, 8 * 8 * 4 * 2);
+        }
 
         printf("\x1b[3;0H");
         for (int y = 0; y < WORLD_HEIGHT; y++)
