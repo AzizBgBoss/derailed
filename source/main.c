e74d9e0a156c6daab75fe7f14a81abed8ba9563a
AzizBgBoss <mohamedazizbenghezala64@gmail.com>
2025-12-29 15:47:33 +0100

    breaking cool

--- source/main.c
+++ source/main.c
@@ -15,9 +15,11 @@
 #define TILE_EMPTY 1
 #define TILE_TREE 2
 #define TILE_ROCK 3
-#define OBJECT_RAIL 4
-#define OBJECT_WOOD 5
-#define OBJECT_IRON 6
+#define OBJECT_RAIL 8
+#define OBJECT_WOOD 9
+#define OBJECT_IRON 10
+#define OBJECT_AXE 11
+#define OBJECT_PICKAXE 12
 
 #define DIR_UP 0
 #define DIR_DOWN 1
@@ -28,6 +30,8 @@ int bg0;
 uint16_t *bg0Map;
 int bg1;
 uint16_t *bg1Map;
+
+unsigned int frames = 0;
 
 int lastPlacedX;
 int lastPlacedY;
@@ -62,6 +66,7 @@ struct Wagon locomotive = {0, 0, 32, 16, DIR_RIGHT, 0.01};
 
 uint8_t worldTerrain[WORLD_WIDTH][WORLD_HEIGHT];
 uint8_t worldObjects[WORLD_WIDTH][WORLD_HEIGHT];
+uint8_t worldHealth[WORLD_WIDTH][WORLD_HEIGHT];
 
 void bg0SetTile(int x, int y, int tile)
 {
@@ -79,6 +84,7 @@ void bg1SetTile(int x, int y, int tile)
 void setWorldTile(int x, int y, int tile)
 {
     worldTerrain[x][y] = tile;
+    worldHealth[x][y] = 3;
     bg0SetTile(x * 2, y * 2, tile * 4);
     bg0SetTile(x * 2 + 1, y * 2, tile * 4 + 1);
     bg0SetTile(x * 2, y * 2 + 1, tile * 4 + 2);
@@ -88,6 +94,24 @@ void setWorldTile(int x, int y, int tile)
 void setWorldObject(int x, int y, int tile)
 {
     worldObjects[x][y] = tile;
+    bg1SetTile(x * 2, y * 2, tile * 4);
+    bg1SetTile(x * 2 + 1, y * 2, tile * 4 + 1);
+    bg1SetTile(x * 2, y * 2 + 1, tile * 4 + 2);
+    bg1SetTile(x * 2 + 1, y * 2 + 1, tile * 4 + 3);
+}
+
+void setWorldHealth(int x, int y, int health)
+{
+    worldHealth[x][y] = health;
+    if (worldHealth[x][y] == 0) {
+        if (worldTerrain[x][y] == TILE_TREE)
+            setWorldObject(x, y, OBJECT_WOOD);
+        else if (worldTerrain[x][y] == TILE_ROCK)
+            setWorldObject(x, y, OBJECT_IRON);
+        setWorldTile(x, y, TILE_EMPTY);
+        return;
+    }
+    int tile = worldTerrain[x][y] + (3 - health) * 2;
     bg1SetTile(x * 2, y * 2, tile * 4);
     bg1SetTile(x * 2 + 1, y * 2, tile * 4 + 1);
     bg1SetTile(x * 2, y * 2 + 1, tile * 4 + 2);
@@ -108,6 +132,9 @@ bool checkCollision(int newX, int newY)
     int right = (newX + PLAYER_SIZE - 1) / TILE_SIZE;
     int top = newY / TILE_SIZE;
     int bottom = (newY + PLAYER_SIZE - 1) / TILE_SIZE;
+
+    if (newX < 0 || newX + PLAYER_SIZE > WORLD_WIDTH * TILE_SIZE || newY < 0 || newY + PLAYER_SIZE > WORLD_HEIGHT * TILE_SIZE)
+        return true;
 
     if ((newX >= locomotive.x && newX < locomotive.x + locomotive.sizeX &&
          newY >= locomotive.y && newY < locomotive.y + locomotive.sizeY) ||
@@ -243,6 +270,9 @@ start:
         setWorldObject(x * 2 + 1, 7, OBJECT_IRON);
     }
 
+    setWorldObject(0, 3, OBJECT_AXE);
+    setWorldObject(1, 3, OBJECT_PICKAXE);
+
     locomotive.x = 0;
     locomotive.y = 5 * TILE_SIZE;
 
@@ -289,6 +319,72 @@ start:
 
         if (!checkCollision(player.x, newY))
             player.y = newY;
+
+        if (frames % 60 == 0)
+        {
+            if (player.objectHeld == OBJECT_AXE)
+            {
+                if (player.direction == DIR_UP)
+                {
+                    if (worldTerrain[((int)player.x + 8) / TILE_SIZE][((int)player.y + 8) / TILE_SIZE - 1] == TILE_TREE)
+                    {
+                        setWorldHealth(((int)player.x + 8) / TILE_SIZE, ((int)player.y + 8) / TILE_SIZE - 1, worldHealth[((int)player.x + 8) / TILE_SIZE][((int)player.y + 8) / TILE_SIZE - 1] - 1);
+                    }
+                }
+                else if (player.direction == DIR_DOWN)
+                {
+                    if (worldTerrain[((int)player.x + 8) / TILE_SIZE][((int)player.y + 8) / TILE_SIZE + 1] == TILE_TREE)
+                    {
+                        setWorldHealth(((int)player.x + 8) / TILE_SIZE, ((int)player.y + 8) / TILE_SIZE + 1, worldHealth[((int)player.x + 8) / TILE_SIZE][((int)player.y + 8) / TILE_SIZE + 1] - 1);
+                    }
+                }
+                else if (player.direction == DIR_LEFT)
+                {
+                    if (worldTerrain[((int)player.x + 8) / TILE_SIZE - 1][((int)player.y + 8) / TILE_SIZE] == TILE_TREE)
+                    {
+                        setWorldHealth(((int)player.x + 8) / TILE_SIZE - 1, ((int)player.y + 8) / TILE_SIZE, worldHealth[((int)player.x + 8) / TILE_SIZE - 1][((int)player.y + 8) / TILE_SIZE] - 1);
+                    }
+                }
+                else if (player.direction == DIR_RIGHT)
+                {
+                    if (worldTerrain[((int)player.x + 8) / TILE_SIZE + 1][((int)player.y + 8) / TILE_SIZE] == TILE_TREE)
+                    {
+                        setWorldHealth(((int)player.x + 8) / TILE_SIZE + 1, ((int)player.y + 8) / TILE_SIZE, worldHealth[((int)player.x + 8) / TILE_SIZE + 1][((int)player.y + 8) / TILE_SIZE] - 1);
+                    }
+                }
+            }
+            else if (player.objectHeld == OBJECT_PICKAXE)
+            {
+                if (player.direction == DIR_UP)
+                {
+                    if (worldTerrain[((int)player.x + 8) / TILE_SIZE][((int)player.y + 8) / TILE_SIZE - 1] == TILE_ROCK)
+                    {
+                        setWorldHealth(((int)player.x + 8) / TILE_SIZE, ((int)player.y + 8) / TILE_SIZE - 1, worldHealth[((int)player.x + 8) / TILE_SIZE][((int)player.y + 8) / TILE_SIZE - 1] - 1);
+                    }
+                }
+                else if (player.direction == DIR_DOWN)
+                {
+                    if (worldTerrain[((int)player.x + 8) / TILE_SIZE][((int)player.y + 8) / TILE_SIZE + 1] == TILE_ROCK)
+                    {
+                        setWorldHealth(((int)player.x + 8) / TILE_SIZE, ((int)player.y + 8) / TILE_SIZE + 1, worldHealth[((int)player.x + 8) / TILE_SIZE][((int)player.y + 8) / TILE_SIZE + 1] - 1);
+                    }
+                }
+                else if (player.direction == DIR_LEFT)
+                {
+                    if (worldTerrain[((int)player.x + 8) / TILE_SIZE - 1][((int)player.y + 8) / TILE_SIZE] == TILE_ROCK)
+                    {
+                        setWorldHealth(((int)player.x + 8) / TILE_SIZE - 1, ((int)player.y + 8) / TILE_SIZE, worldHealth[((int)player.x + 8) / TILE_SIZE - 1][((int)player.y + 8) / TILE_SIZE] - 1);
+                    }
+                }
+                else if (player.direction == DIR_RIGHT)
+                {
+                    if (worldTerrain[((int)player.x + 8) / TILE_SIZE + 1][((int)player.y + 8) / TILE_SIZE] == TILE_ROCK)
+                    {
+                        setWorldHealth(((int)player.x + 8) / TILE_SIZE + 1, ((int)player.y + 8) / TILE_SIZE, worldHealth[((int)player.x + 8) / TILE_SIZE + 1][((int)player.y + 8) / TILE_SIZE] - 1);
+                    }
+                }
+            }
+        }
 
         // Select object automatically
 
@@ -400,6 +496,8 @@ start:
         printf("x: %f, y: %f, dir: %d, obj: %d\n", player.x, player.y, player.direction, player.selectedObject);
         printf("x: %d, y: %d, object: %d\n", player.selectedObjectX, player.selectedObjectY, worldObjects[player.selectedObjectX][player.selectedObjectY]);
         printf("obj held: %d, quantity: %d\n", player.objectHeld, player.quantityHeld);
+
+        frames++;
     }
 
     return 0;
